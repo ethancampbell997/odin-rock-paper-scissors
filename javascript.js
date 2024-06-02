51dd84008b179d32cf46d8116a3f56dfec878ae2
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-06-02 14:22:39 -0400

    Write pseudocode to get computer and human input

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello world")
+/*PSEUDOCODE 
+getComputerChoice function
+    random number 1-3
+    assign 1 to "ROCK," 2 to "PAPER," and 3 to "SCISSORS"
+    return the string
+getHumanChoice function
+    prompt the user for input (rock, paper, or scissors)
+    set the string to all caps
+    return the string
+*/
