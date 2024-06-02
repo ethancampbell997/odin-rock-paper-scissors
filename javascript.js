ee094251b394080dffd3c03159a5e9c92e034d5c
Ethan Campbell <ethancampbell997@users.noreply.github.com>
2024-06-02 14:52:02 -0400

    Implement getComputerChoice + getHumanChoice

--- javascript.js
+++ javascript.js
@@ -8,3 +8,26 @@ getHumanChoice function
     set the string to all caps
     return the string
 */
+//randomly returns string of ROCK, PAPER, or SCISSORS
+function getComputerChoice(){
+    let ranNum = Math.floor(Math.random() * 3) + 1;
+    let str;
+    if(ranNum === 1){
+        str = "ROCK";
+    }//if
+    else if(ranNum === 2){
+        str = "PAPER";
+    }//else if
+    else{
+        str = "SCISSORS"
+    }//else
+    return str;
+}//getComputerChoice
+console.log(getComputerChoice());
+//returns capitalized version of user input
+function getHumanChoice(){
+    let str = prompt("Enter rock, paper, or scissors:");
+    str = str.toUpperCase();
+    return str;
+}//getHumanChoice
+console.log(getHumanChoice());
